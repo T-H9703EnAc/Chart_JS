fdbf3ddfb06bb7fe2cf7e3a8df06e989130e25ce
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2023-06-22 16:03:00 +0900

    新規作成

---
+++ js/simple_chart_display.js
@@ -0,0 +1,66 @@
+/**
+ * コンストラクタ
+ */
+function SimpleChartDisplay() {
+  this.chart = null;
+}
+
+/**
+ * JSを読み込み時に行う処理
+ */
+$(document).ready(function () {
+  new SimpleChartDisplay().init();
+});
+
+/**
+ * 最初に行う処理
+ */
+SimpleChartDisplay.prototype.init = function () {
+  let panel = this;
+  $("#displayBtn").click(function () {
+    panel.chartDisplay();
+  });
+};
+
+/**
+ * チャート表示処理
+ */
+SimpleChartDisplay.prototype.chartDisplay = function () {
+  // canvasから2D描画コンテキストを取得
+  const ctx = $("#chart")[0].getContext("2d");
+  // チャートを描画する。
+  this.chart = new Chart(ctx, this.getChartProperty());
+};
+
+/**
+ * チャートを描画するためのプロパティの取得
+ * @returns チャート描画プロパティ
+ */
+SimpleChartDisplay.prototype.getChartProperty = function () {
+  let chartProperty = {
+    // グラフの種類(今回は棒グラフ)
+    type: "bar",
+    // データ
+    data: this.getChartData(),
+  };
+  return chartProperty;
+};
+
+/**
+ * チャートに表示するデータのプロパティを取得
+ * @returns チャートデータ
+ */
+SimpleChartDisplay.prototype.getChartData = function () {
+  let chartData = {
+    // x軸のラベル
+    labels: ["HP", "こうげき", "ぼうぎょ", "すばやさ", "とくぼう", "とくこう"],
+    datasets: [
+      {
+        label: "ピカチュウ",
+        // グラフのデータ
+        data: [211, 146, 116, 194, 136, 149],
+      },
+    ],
+  };
+  return chartData;
+};
